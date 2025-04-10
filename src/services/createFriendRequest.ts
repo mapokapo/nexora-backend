06e8614b8efadd327da78eab8f9a07e823c6f961
mapokapo <leopetrovic11@gmail.com>
2025-04-10 18:17:32 +0200

    fix: add support for mutual friend requests

--- src/services/createFriendRequest.ts
+++ src/services/createFriendRequest.ts
@@ -10,10 +10,35 @@ export const createFriendRequest = async (
   const friendRequests = await firestore
     .doc(`friendRequests/${user.uid}`)
     .get();
-  const sentRequests = friendRequests.get("sentRequests") as string[];
+  const otherFriendRequests = await firestore
+    .doc(`friendRequests/${otherUserId}`)
+    .get();
+  const otherUserProfile = await firestore.doc(`profiles/${otherUserId}`).get();
 
+  const sentRequests = friendRequests.get("sentRequests") as string[];
   if (sentRequests.includes(otherUserId)) {
     throw new Error("Friend request already sent");
+  }
+
+  const alreadyFriends = (
+    friendRequests.get("allFriends") as string[]
+  ).includes(otherUserId);
+  if (alreadyFriends) {
+    throw new Error("You are already friends with this user");
+  }
+
+  const allowFriendRequestsFrom = (
+    otherUserProfile.get("settings") as Record<string, string>
+  ).allowFriendRequestsFrom;
+  const otherUserFriends = otherFriendRequests.get("allFriends") as string[];
+  const isMutual = otherUserFriends.some(uid =>
+    (friendRequests.get("allFriends") as string[]).includes(uid)
+  );
+  if (
+    allowFriendRequestsFrom === "nobody" ||
+    (allowFriendRequestsFrom === "mutuals" && !isMutual)
+  ) {
+    throw new Error("You cannot befriend this user");
   }
 
   await firestore.doc(`friendRequests/${user.uid}`).update({
